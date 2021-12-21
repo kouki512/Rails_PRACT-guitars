da723886871fc4eb62924111712da3625629dec2
kouki512 <ht20a008@oecu.jp>
2021-12-21 14:22:05 +0900

    [Start]make-users

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :practices
   resources :users
+
   root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

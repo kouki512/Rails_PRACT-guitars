0443b06c8f49445f5f72858abd766c22a9115aa4
kouki512 <ht20a008@oecu.jp>
2021-12-21 18:14:48 +0900

    [start]user-show design

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  attachment :profile_image
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :practices, dependent: :destroy

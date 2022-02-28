fad2a699269e30970a85c2672f1e2785b34144a9
kouki512 <ht20a008@oecu.jp>
2022-02-28 22:39:37 +0900

    [update]ver1.1.0

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,12 +1,12 @@
 require "refile/s3"
 
-aws = {
-  access_key_id: ENV['AKIAWRSUNCEYB6WECPFD
-  '],
-  secret_access_key: ENV['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
-  '],
-  region: "ap-northeast-1",
-  bucket: "pract-guitars",
-}
-Refile.cache = Refile::S3.new(prefix: "cache", **aws)
-Refile.store = Refile::S3.new(prefix: "store", **aws)
+if Rails.env.production? # 本番環境の場合はS3へアップロード
+  aws = {
+    access_key_id: ENV['AKIAWRSUNCEYB6WECPFD'], # アクセスキーID
+    secret_access_key: ENV['wP9S1OcJR96VxrQzFynmS/yUcxHZSmBnaogFXtQZ'], # シークレットアクセスキー
+    region: 'ap-northeast-1', # リージョン
+    bucket: 'pract-guitars', # バケット名
+  }
+  Refile.cache = Refile::S3.new(prefix: 'cache', **aws)
+  Refile.store = Refile::S3.new(prefix: 'store', **aws)
+end

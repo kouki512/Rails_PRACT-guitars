1b1ee6109a7a2ec8517bd5accc54cc2cdec4a130
KokiIshida <ht20a008@oecu.jp>
2022-03-01 19:25:43 +0900

    [change]refile.rb

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -4,9 +4,9 @@ if Rails.env.production? # 本番環境の場合はS3へアップロード
   aws = {
     access_key_id: ENV['AWS_ACCESS_KEY_ID'], # アクセスキーID
     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # シークレットアクセスキー
-    region: 'ap-northeast-1', # リージョン
-    bucket: 'pract-guitars' # バケット名
+    region: ENV['AWS_REGION'], # リージョン
+    bucket: ENV['AWS_BUCKET'] # バケット名
   }
-  Refile.cache = Refile::S3.new(prefix: "cache", **aws)
-  Refile.store = Refile::S3.new(prefix: "store", **aws)
+  Refile.cache = Refile::S3.new(prefix: "cache", **aws,http_wire_trace: true)
+  Refile.store = Refile::S3.new(prefix: "store", **aws,http_wire_trace: true)
 end

7bea8bf20aa6a5ecce4f0cbdad64d0adf4599391
KokiIshida <ht20a008@oecu.jp>
2022-03-01 15:04:20 +0900

    [change]deploying method

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,10 +1,7 @@
 require "refile/s3"
-
-if Rails.env.production? # 本番環境の場合はS3へアップロード
   aws = {
     access_key_id: ENV['AKIAWRSUNCEYBDINTOXV'], # アクセスキーID
     secret_access_key: ENV['N/LCsJZjx4ugBuGjkLidUBYGCgjXFXdqnAT9tRTG'], # シークレットアクセスキー
     region: 'ap-northeast-1', # リージョン
     bucket: 'pract-guitars', # バケット名
   }
-end
